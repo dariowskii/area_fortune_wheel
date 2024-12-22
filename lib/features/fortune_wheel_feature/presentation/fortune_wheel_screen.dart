833536421fa5e7a521671fc6725519f7e24e5add
Dario Varriale <dario.varriale@atobit.it>
2024-12-22 18:37:59 +0100

    Update confetti

--- lib/features/fortune_wheel_feature/presentation/fortune_wheel_screen.dart
+++ lib/features/fortune_wheel_feature/presentation/fortune_wheel_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:math' as math;
 
+import 'package:confetti/confetti.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_fortune_wheel/flutter_fortune_wheel.dart';
 
@@ -12,11 +13,15 @@ class FortuneWheelScreen extends StatefulWidget {
 }
 
 class _FortuneWheelScreenState extends State<FortuneWheelScreen> {
-  StreamController<int> selected = StreamController<int>();
+  late final _selectedStream = StreamController<int>();
+  late final _confettiController = ConfettiController(
+    duration: const Duration(seconds: 5),
+  );
 
   @override
   void dispose() {
-    selected.close();
+    _selectedStream.close();
+    _confettiController.dispose();
     super.dispose();
   }
 
@@ -24,78 +29,121 @@ class _FortuneWheelScreenState extends State<FortuneWheelScreen> {
   Widget build(BuildContext context) {
     final size = MediaQuery.sizeOf(context);
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Arena\'s Fortune Wheel'),
+      ),
       body: SafeArea(
-        child: Row(
+        child: Stack(
           children: [
-            Expanded(
-              child: LayoutBuilder(
-                builder: (context, constraints) {
-                  return Column(
-                    children: [
-                      SizedBox(
-                        height: constraints.maxHeight * 0.8,
-                        width: constraints.maxHeight * 0.8,
-                        child: FortuneWheel(
-                          selected: selected.stream,
-                          alignment: Alignment.centerRight,
-                          indicators: [
-                            FortuneIndicator(
+            Row(
+              children: [
+                Expanded(
+                  child: LayoutBuilder(
+                    builder: (context, constraints) {
+                      return Column(
+                        children: [
+                          SizedBox(
+                            height: constraints.maxHeight * 0.8,
+                            width: constraints.maxHeight * 0.8,
+                            child: FortuneWheel(
+                              selected: _selectedStream.stream,
                               alignment: Alignment.centerRight,
-                              child: Transform.rotate(
-                                angle: math.pi / 2,
-                                child: TriangleIndicator(),
-                              ),
+                              animateFirst: false,
+                              onAnimationEnd: () {
+                                _confettiController.play();
+                              },
+                              onAnimationStart: () {
+                                _confettiController.stop();
+                              },
+                              indicators: [
+                                FortuneIndicator(
+                                  alignment: Alignment.centerRight,
+                                  child: Transform.rotate(
+                                    angle: math.pi / 2,
+                                    child: TriangleIndicator(),
+                                  ),
+                                ),
+                              ],
+                              items: [
+                                FortuneItem(child: Text('Item 1')),
+                                FortuneItem(child: Text('Item 2')),
+                                FortuneItem(child: Text('Item 3')),
+                                FortuneItem(child: Text('Item 4')),
+                                FortuneItem(child: Text('Item 5')),
+                                FortuneItem(child: Text('Item 6')),
+                                FortuneItem(child: Text('Item 7')),
+                                FortuneItem(child: Text('Item 8')),
+                                FortuneItem(child: Text('Item 9')),
+                                FortuneItem(child: Text('Item 10')),
+                              ],
                             ),
-                          ],
-                          items: [
-                            FortuneItem(child: Text('Item 1')),
-                            FortuneItem(child: Text('Item 2')),
-                            FortuneItem(child: Text('Item 3')),
-                            FortuneItem(child: Text('Item 4')),
-                            FortuneItem(child: Text('Item 5')),
-                            FortuneItem(child: Text('Item 6')),
-                            FortuneItem(child: Text('Item 7')),
-                            FortuneItem(child: Text('Item 8')),
-                            FortuneItem(child: Text('Item 9')),
-                            FortuneItem(child: Text('Item 10')),
-                          ],
+                          ),
+                          SizedBox(height: 64),
+                          ElevatedButton(
+                            onPressed: () {
+                              final random = math.Random();
+                              _selectedStream.add(random.nextInt(10));
+                            },
+                            child: Text('Spin'),
+                          ),
+                        ],
+                      );
+                    },
+                  ),
+                ),
+                SizedBox(
+                  width: size.width * 0.2,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Partecipanti',
+                        style: Theme.of(context).textTheme.labelLarge,
+                      ),
+                      SizedBox(height: 16),
+                      Expanded(
+                        child: ListView.builder(
+                          itemCount: 10,
+                          itemBuilder: (context, index) {
+                            return ListTile(
+                              title: Text('Partecipante ${index + 1}'),
+                            );
+                          },
                         ),
                       ),
-                      SizedBox(height: 64),
-                      ElevatedButton(
-                        onPressed: () {
-                          final random = math.Random();
-                          selected.add(random.nextInt(10));
-                        },
-                        child: Text('Spin'),
+                      SizedBox(height: 16),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 16),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            hintText: 'Aggiungi partecipanti',
+                            border: OutlineInputBorder(),
+                          ),
+                          onTapOutside: (_) {
+                            FocusScope.of(context).unfocus();
+                          },
+                          maxLength: 1000,
+                          minLines: 1,
+                          maxLines: 10,
+                          onSubmitted: (value) {
+                            print(value);
+                          },
+                        ),
                       ),
                     ],
-                  );
-                },
+                  ),
+                )
+              ],
+            ),
+            Align(
+              alignment: Alignment.topCenter,
+              child: ConfettiWidget(
+                confettiController: _confettiController,
+                blastDirectionality: BlastDirectionality.explosive,
+                blastDirection: math.pi,
+                numberOfParticles: 200,
               ),
             ),
-            SizedBox(
-              width: size.width * 0.2,
-              child: Column(
-                children: [
-                  Text(
-                    'Partecipanti',
-                    style: Theme.of(context).textTheme.labelLarge,
-                  ),
-                  SizedBox(height: 16),
-                  Expanded(
-                    child: ListView.builder(
-                      itemCount: 10,
-                      itemBuilder: (context, index) {
-                        return ListTile(
-                          title: Text('Partecipante ${index + 1}'),
-                        );
-                      },
-                    ),
-                  ),
-                ],
-              ),
-            )
           ],
         ),
       ),
