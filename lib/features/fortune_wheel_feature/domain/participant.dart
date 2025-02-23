29ab8ecec2b32865c2d005bd4f9d25cd2ee566a9
Dario Varriale <dario.varriale@atobit.it>
2025-02-23 17:07:59 +0100

    Add UUID to Partecipant model and update named factory method

--- lib/features/fortune_wheel_feature/domain/participant.dart
+++ lib/features/fortune_wheel_feature/domain/participant.dart
@@ -1,4 +1,5 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:uuid/uuid.dart';
 
 part 'participant.freezed.dart';
 part 'participant.g.dart';
@@ -6,11 +7,15 @@ part 'participant.g.dart';
 @freezed
 class Partecipant with _$Partecipant {
   const factory Partecipant({
+    required String uuid,
     required String name,
   }) = _Partecipant;
 
   factory Partecipant.fromJson(Map<String, dynamic> json) =>
       _$PartecipantFromJson(json);
 
-  factory Partecipant.named(String name) => Partecipant(name: name);
+  factory Partecipant.named(String name) => Partecipant(
+        uuid: const Uuid().v4(),
+        name: name,
+      );
 }
