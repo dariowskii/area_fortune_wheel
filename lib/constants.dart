6b4eba67e2103f115de09b734ce479f0bba639ea
Dario Varriale <dario.varriale@atobit.it>
2025-02-23 17:07:51 +0100

    Add predefined spacings

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,11 +2,25 @@ import 'package:arena_fortune_wheel/features/fortune_wheel_feature/domain/partic
 import 'package:flutter/material.dart';
 
 const kDefaultItems = [
-  Partecipant(name: 'Inserisci due persone'),
-  Partecipant(name: 'per usare la ruota!'),
+  Partecipant(uuid: '', name: 'Inserisci due persone'),
+  Partecipant(uuid: '', name: 'per usare la ruota!'),
 ];
 
 const kDefaultItemsColors = [
   Colors.red,
   Colors.blue,
 ];
+
+// SPACING
+
+const kWidth4 = SizedBox(width: 4);
+const kWidth8 = SizedBox(width: 8);
+const kWidth16 = SizedBox(width: 16);
+const kWidth24 = SizedBox(width: 24);
+const kWidth32 = SizedBox(width: 32);
+
+const kHeight4 = SizedBox(height: 4);
+const kHeight8 = SizedBox(height: 8);
+const kHeight16 = SizedBox(height: 16);
+const kHeight24 = SizedBox(height: 24);
+const kHeight32 = SizedBox(height: 32);
